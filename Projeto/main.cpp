99180552808ad851e51e564e822678822f7d5729
gbvsCloud <gabriielblock@gmail.com>
2023-03-06 05:31:23 -0300

    att

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -58,7 +58,14 @@ struct Player{
 	
 	bool isMoving;
 	
+	char moves[40];
+	int movesPending;
+	
+	bool wPress, sPress, aPress, dPress;
+	
+	
 	Player(){
+		movesPending = 0;
 		maxLife = 1;
 		life = 1;
 		x = display_width / 2 - 64;
@@ -68,28 +75,79 @@ struct Player{
 	}
 	
 	
-	bool IsPressing(){
-		if(GetKeyState(key_W) & 0x80 || GetKeyState(key_S) & 0x80 || GetKeyState(key_A) & 0x80 || GetKeyState(key_D) & 0x80)
+	bool IsPressing(int _key){
+		if(GetKeyState(_key) & 0x80)
 			return true;
 		else 
 			return false;
 	}	
 	
 	
-	void Movement(){
-		if(GetKeyState(key_W) & 0x80 && !isMoving){
-			y -= speed;
-		} 
-		else if(GetKeyState(key_S) & 0x80 && !isMoving){
-			y += speed;
-		} 
-		else if(GetKeyState(key_A) & 0x80 && !isMoving){
-			x -= speed;
-		} 
-		else if(GetKeyState(key_D) & 0x80 && !isMoving){
-			x += speed;
+	void RemoveFirstMove(){
+		for(int i = 0; i < movesPending; i++){
+			moves[i] =  moves[i+1];
 		}
-			isMoving = IsPressing();
+	}
+	void Movement(){
+		
+		if(movesPending < 41){
+			if(GetKeyState(key_W) & 0x80 && !wPress){
+				moves[movesPending] = 'w';
+				wPress = true;
+				movesPending++;
+			}
+			if(GetKeyState(key_S) & 0x80 && !sPress){
+				moves[movesPending] = 's';
+				movesPending++;
+				sPress = true;
+			} 
+			if(GetKeyState(key_A) & 0x80 && !aPress){
+				moves[movesPending] = 'a';
+				movesPending++;
+				aPress = true;
+			} 
+			if(GetKeyState(key_D) & 0x80 && !dPress){
+				moves[movesPending] = 'd';
+				movesPending++;
+				dPress = true;
+			}	
+		}
+		
+		wPress = IsPressing(key_W);
+		sPress = IsPressing(key_S);
+		aPress = IsPressing(key_A);
+		dPress = IsPressing(key_D);
+		
+		if(movesPending > 0){
+			switch(moves[0]){
+				case('w'):
+					y -= 64;
+					movesPending--;
+					RemoveFirstMove();					
+					break;
+				case('s'):
+					y += 64;
+					movesPending--;
+					RemoveFirstMove();					
+					break;
+				case('a'):
+					x -= 64;
+					movesPending--;
+					RemoveFirstMove();					
+					break;
+				case('d'):
+					x += 64;
+					movesPending--;
+					RemoveFirstMove();					
+					break;
+				
+			}
+		}
+		
+		
+		
+	
+	
 	}
 	
 };
@@ -188,93 +246,84 @@ Enemy AddEnemy(int speed, char *spriteAddress, int direction, int layer, int x){
 	return enemy;
 }
 
-struct fase{
-	
-	
-};
-	
 
-int main() {
-		
-	initwindow(display_width, display_heigth, "Gatonauta");
+void fase1(){
+	
+	Player player;
+	player.x = display_width / 2 - 64;
+	player.y = display_heigth - 64;
 	
 	int enemyQuantity = 17;
+	int enemyAdded = 0;
 	Enemy *enemies = (Enemy *) malloc(sizeof(Enemy) * enemyQuantity);
-	
-	
-	
-	for(int i =0; i < 4; i++)
-		enemies[i] = AddEnemy(6, "car.jpg", 0, 9, i);
-	for(int i =0; i < 4; i++)
-		enemies[i+4] = AddEnemy(6, "car.jpg", 0, 9, i+8);
-		
-	for(int i =0; i < 2; i++)
-		enemies[i+8] = AddEnemy(5, "car.jpg", 0, 8, i+2);
-	for(int i =0; i < 2; i++)
-		enemies[i+10] = AddEnemy(5, "car.jpg", 0, 8, i+10);
-	
-	for(int i =0; i < 2; i++)
-		enemies[i+12] = AddEnemy(4, "car.jpg", 0, 7, i);
-	for(int i =0; i < 2; i++)
-		enemies[i+14] = AddEnemy(4, "car.jpg", 0, 7, i+15);
-
-	enemies[16] = AddEnemy(60, "car.jpg", 0, 6, 0);
 	
 	void *grass = LoadSprite("grass.jpg", 32, 32, 2);
 	void *water = LoadSprite("water.jpg", 32, 32, 2);	
-	
-	
-	
-	
 	void *tiles[2] = {grass, water};
-	
-	setvisualpage(pg);
-	setactivepage(pg);
 	
 	unsigned long tickCount;
 	unsigned long initTick;
-	initTick = GetTickCount();
-	
-
-	//variaveis do player
-
-	int x = 0;
-	int y = 0;
 	
 	int ticks = 0;
 	
-	Player player;
+	initTick = GetTickCount();
 	
+	for(int i =0; i < 4; i++){
+		enemies[enemyAdded] = AddEnemy(6, "car.jpg", 0, 9, i);
+		enemyAdded++;
+	}		
+	for(int i =0; i < 4; i++){
+		enemies[enemyAdded] = AddEnemy(6, "car.jpg", 0, 9, i+8);
+		enemyAdded++;
+	}	
+	for(int i =0; i < 2; i++){
+		enemies[enemyAdded] = AddEnemy(5, "car.jpg", 0, 8, i+2);
+		enemyAdded++;
+	}	
+	for(int i =0; i < 2; i++){
+		enemies[enemyAdded] = AddEnemy(5, "car.jpg", 0, 8, i+10);
+		enemyAdded++;
+	}		
+	
+	for(int i =0; i < 2; i++){
+		enemies[enemyAdded] = AddEnemy(4, "car.jpg", 0, 7, i);
+		enemyAdded++;
+	}
 		
+	for(int i =0; i < 2; i++){
+		enemies[enemyAdded] = AddEnemy(4, "car.jpg", 0, 7, i+15);
+		enemyAdded++;
+	}
+		
+
+	enemies[16] = AddEnemy(60, "car.jpg", 0, 6, 0);
+	
 	while(true){
 		tickCount = GetTickCount();
-			
 		if(initTick - tickCount > 1000/60){
 			if(pg == 1) pg = 2; else pg = 1;
-			initTick = tickCount;
-			
+			initTick = tickCount;			
 			ticks++;
 			
-			//if(ticks > 60)	
-			//	printf("\nDistance:%i", abs(enemies[0].x - player.x) + abs(enemies[0].y - player.y));
 
 			setvisualpage(pg);
 			cleardevice();
 			RenderTilemap(tiles);
 			
-
-			
-			
 			player.Movement();
+			
+
 			for(int i = 0; i < enemyQuantity; i++){
 				enemies[i].Draw();
 				enemies[i].Movement(ticks);
+				enemies[i].CheckCollision(player.x, player.y, 1, 1);
 				if(enemies[i].CheckCollision(player.x, player.y, 1, 1)){
 					player.x = display_width / 2 - 64;
-					player.y = display_heigth - 64;
+					player.y = display_heigth - 64;	
 				}
 					
-			}	
+			}
+				
 			
 
 				
@@ -282,6 +331,33 @@ int main() {
 			setactivepage(pg);
 			if(ticks > 60) ticks = 0;
 		}
+		
+		
+		
+	}
+
+}
+	
+
+int main() {
+		
+	initwindow(display_width, display_heigth, "Gatonauta");
+	
+	
+
+		
+	setvisualpage(pg);
+	setactivepage(pg);
+	
+	
+	
+
+
+
+		
+	while(true){
+	
+		fase1();
 			
 	}
 	
