17dc0fc43c107140db6a9b1833808bce2a4365fd
gbvsCloud <gabriielblock@gmail.com>
2023-04-21 14:07:49 -0300

    att

--- Gatonauta 2.0/main.cpp
+++ Gatonauta 2.0/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <graphics.h>
 #include <math.h>
+#include <time.h>
 
 #define display_width 1680
 
@@ -23,7 +24,9 @@ int pg = 1;
 
 enum Gamestate{
 	MainMenu,
-	Gameplay,
+	Level1,
+	Level2,
+	Level3,
 	Dead,
 	Win	
 };
@@ -54,9 +57,15 @@ int gameplayTimer[2];
 struct Cat{
 	int x;
 	int y;
-	int xSize;
-	int ySize;
+	bool unlocked;
 	void* sprite;
+};
+
+struct Button{
+	int x;
+	int y;
+	void* sprite;
+	
 };
 
 struct Alien{
@@ -114,12 +123,32 @@ void** LoadSpriteSheet(int spriteQuantity, const char* spriteAddress, int sprite
 	return image;
 }
 
+Button AddButton(void* sprite){
+	Button button;
+	button.sprite = sprite;
+	do{
+		button.x = (rand() % 13) * 128;
+		button.y = (rand() % 6 + 1) * 128;		
+	}while((button.x == display_width / 2 + 64 && button.y == display_height - 128) || (button.x == display_width / 2 + 64 && button.y == 128));
+	
+	return button;
+}
 
+Cat AddCat(int x, int y, void* sprite){
+	Cat cat;
+	cat.x = x * 128;
+	cat.y = y * 128 + 26;
+	cat.sprite = sprite;
+	cat.unlocked = false;
+	
+	return cat;
+}
 
 Alien AddAlien(int x, int y, int direction, int speed, void* sprite, int xSize, int ySize){
 	Alien alien;
 	alien.x = x * 128;
 	alien.y = y * 128 + 26;
+	
 	alien.direction = direction;
 	alien.speed = speed;
 	alien.sprite = sprite;
@@ -295,7 +324,7 @@ void MainMenuState(){
 		
 		putimage(0, 0, menuBg, COPY_PUT);
 		
-		if(kbhit()) gs = Gameplay;
+		if(kbhit()) gs = Level1;
 		
 		
 		setactivepage(pg);
@@ -307,7 +336,7 @@ void DrawSprite(int x, int y, void* sprite){
 	putimage(x, y, sprite, COPY_PUT);
 }
 
-void GameplayState(){
+void Level3State(){
 	
 	unsigned long tickCount;
 	unsigned long initTick;
@@ -318,22 +347,31 @@ void GameplayState(){
 	
 	void* background = LoadSprite("map2.jpg", 1600, 900, 1, 0, 0, 1600, 900);
 	
-	void* playerSprite;
-	
 	Alien* aliens;
-	int alienQuantity = 1;
+	int alienQuantity = 4;
 	aliens = NULL;	
 	void* alienImage = LoadSprite("alien.jpg", 128, 128, 1);	
 	aliens = (Alien *) realloc(aliens, sizeof(Alien) * alienQuantity);
-	aliens[0] = AddAlien(0, 5, 1, 1, alienImage, 1, 1);
+	aliens[0] = AddAlien(0, 5, 1, 2, alienImage, 1, 1);
+	aliens[1] = AddAlien(13, 2, -1, 3, alienImage, 1, 1);
+	aliens[2] = AddAlien(0, 4, 1, 5, alienImage, 1, 1);
+	aliens[3] = AddAlien(13, 6, 1, 4, alienImage, 1, 1);
 	
-	Cat cat;
 	void* catImage = LoadSprite("gato.jpg", 128, 128);
-	cat.sprite = catImage;
+	Cat cat = AddCat(6, 1, catImage);
+	void* buttonImage = LoadSprite("button.jpg", 128, 128);
 	
-	cat.x = display_width / 2 - tilesize;
-	cat.y = 0;
 	
+	Button* buttons;
+	int buttonQuantity = 6;
+	int buttonPressed = 0;
+	buttons = NULL;
+	buttons = (Button *) realloc(buttons, sizeof(Button) * buttonQuantity);
+	for(int i = 0; i < buttonQuantity; i++)
+		buttons[i] = AddButton(buttonImage);
+	
+	
+	/*
 	Meteor* meteors;
 	int meteorQuantity = 4;
 	void* meteorImage = LoadSprite("car.jpg", 128, 128, 1);
@@ -343,39 +381,51 @@ void GameplayState(){
 	meteors[0] = AddMeteor(0, 3, 1, 2, meteorImage, 1, 1);
 	meteors[1] = AddMeteor(12, 2, -1, 3, meteorImage, 1, 1);
 	meteors[2] = AddMeteor(0, 1, 1, 4, meteorImage, 1, 1);
-	meteors[3] = AddMeteor(12, 0, -1, 1, meteorImage, 1, 1);
+	meteors[3] = AddMeteor(12, 0, -1, 1, meteorImage, 1, 1);*/
 	
-	int tileset[7] = {1, 1, 1, 1, 0, 0, 0};
+	int tileset[7] = {0, 0, 0, 0, 0, 0, 0};
 	
-	score = 0;
 	
 	gameplayTimer[0] = 6;
 	gameplayTimer[1] = 0;
 	
 	int nDigits = floor(log10(abs(139))) + 1;
-	printf("%i", nDigits);
 	
-	while(gs == Gameplay){
+
+	while(gs == Level3){
 		tickCount = GetTickCount();
 		if(initTick - tickCount > 1000/60){
 			initTick = tickCount;
 			cicleCounter++;
 			SwitchPage();
-			playerSprite = playerSheet[activeSprite];
 			putimage(0, 0, background, COPY_PUT);
 			
-		
 			
 			//TIMER VISUAL
 			putimage(display_width / 2-128, 0, numbers[gameplayTimer[0]], COPY_PUT);
 			putimage(display_width / 2, 0, numbers[gameplayTimer[1]], COPY_PUT);
 			
-			printf("%i\n", aliens[0].x);
 			if(tileset[playerY / 128] == 1)
 				onMeteor = false;
 			
-				DrawSprite(cat.x, cat.y, cat.sprite);
+			DrawSprite(cat.x, cat.y, cat.sprite);
+			//PASSAR DE FASE
+			if(CheckCollision(cat.x, cat.y, 1, 1) && cat.unlocked){
+				gs = Level1;
+				PlayerReturn();
+			};
 			
+			
+			for(int i = 0; i < buttonQuantity; i++){
+				DrawSprite(buttons[i].x, buttons[i].y, buttons[i].sprite);	
+				if(CheckCollision(buttons[i].x, buttons[i].y, 1, 1)){
+					buttons[i].y = - 128;	
+					buttonPressed++;				
+				}
+							
+			}
+				
+			/*
 			for(int i =0; i < meteorQuantity; i++){
 				
 				if(cicleCounter % (60 / meteors[i].speed) == 0){
@@ -400,8 +450,9 @@ void GameplayState(){
 					else
 						meteors[i].x = display_width - tilesize - 8;
 				}	
-			}
+			}*/
 			
+			//DRAW E UPDATE DOS ALEINS
 			for(int i =0; i < alienQuantity; i++){
 				if(cicleCounter % (60 / aliens[i].speed) == 0) aliens[i].x += tilesize * aliens[i].direction;
 				DrawSprite(aliens[i].x, aliens[i].y, aliens[i].sprite);
@@ -413,10 +464,9 @@ void GameplayState(){
 						aliens[i].x = display_width - tilesize - 8;
 				}	
 			}
-			
 			PlayerInput();
 			PlayerMovement();	
-			DrawSprite(playerX, playerY, playerSprite);
+			DrawSprite(playerX, playerY, playerSheet[activeSprite]);
 			
 			if(IsPressing(key_ESCAPE)){
 				playing = false;
@@ -427,6 +477,10 @@ void GameplayState(){
 				PlayerReturn();
 				onMeteor = true;
 			} 
+			
+			if(buttonPressed >= buttonQuantity){
+				cat.unlocked = true;
+			}
 			
 			setactivepage(pg);
 			if(cicleCounter >= 60){
@@ -443,8 +497,353 @@ void GameplayState(){
 		}
 	}
 	free(aliens);
-	free(meteors);
+	free(background);
+	free(catImage);
+	free(alienImage);
+	free(buttonImage);
+	//free(meteors);
 }
+
+void Level2State(){
+	
+	unsigned long tickCount;
+	unsigned long initTick;
+	
+	initTick = GetTickCount();
+	
+	int cicleCounter = 0;
+	
+	void* background = LoadSprite("map2.jpg", 1600, 900, 1, 0, 0, 1600, 900);
+	
+	Alien* aliens;
+	int alienQuantity = 4;
+	aliens = NULL;	
+	void* alienImage = LoadSprite("alien.jpg", 128, 128, 1);	
+	aliens = (Alien *) realloc(aliens, sizeof(Alien) * alienQuantity);
+	aliens[0] = AddAlien(0, 5, 1, 2, alienImage, 1, 1);
+	aliens[1] = AddAlien(13, 2, -1, 3, alienImage, 1, 1);
+	aliens[2] = AddAlien(0, 4, 1, 5, alienImage, 1, 1);
+	aliens[3] = AddAlien(13, 6, 1, 4, alienImage, 1, 1);
+	
+	void* catImage = LoadSprite("gato.jpg", 128, 128);
+	Cat cat = AddCat(6, 1, catImage);
+	void* buttonImage = LoadSprite("button.jpg", 128, 128);
+	
+	
+	Button* buttons;
+	int buttonQuantity = 4;
+	int buttonPressed = 0;
+	buttons = NULL;
+	buttons = (Button *) realloc(buttons, sizeof(Button) * buttonQuantity);
+	for(int i = 0; i < buttonQuantity; i++)
+		buttons[i] = AddButton(buttonImage);
+	
+	
+	/*
+	Meteor* meteors;
+	int meteorQuantity = 4;
+	void* meteorImage = LoadSprite("car.jpg", 128, 128, 1);
+	meteors = NULL;
+	
+	meteors = (Meteor *) realloc(meteors, sizeof(Meteor) * meteorQuantity);
+	meteors[0] = AddMeteor(0, 3, 1, 2, meteorImage, 1, 1);
+	meteors[1] = AddMeteor(12, 2, -1, 3, meteorImage, 1, 1);
+	meteors[2] = AddMeteor(0, 1, 1, 4, meteorImage, 1, 1);
+	meteors[3] = AddMeteor(12, 0, -1, 1, meteorImage, 1, 1);*/
+	
+	int tileset[7] = {0, 0, 0, 0, 0, 0, 0};
+	
+	
+	gameplayTimer[0] = 6;
+	gameplayTimer[1] = 0;
+	
+	int nDigits = floor(log10(abs(139))) + 1;
+	
+	while(gs == Level2){
+		tickCount = GetTickCount();
+		if(initTick - tickCount > 1000/60){
+			initTick = tickCount;
+			cicleCounter++;
+			SwitchPage();
+			putimage(0, 0, background, COPY_PUT);
+			
+			
+			//TIMER VISUAL
+			putimage(display_width / 2-128, 0, numbers[gameplayTimer[0]], COPY_PUT);
+			putimage(display_width / 2, 0, numbers[gameplayTimer[1]], COPY_PUT);
+			
+			if(tileset[playerY / 128] == 1)
+				onMeteor = false;
+			
+			DrawSprite(cat.x, cat.y, cat.sprite);
+			//PASSAR DE FASE
+			if(CheckCollision(cat.x, cat.y, 1, 1) && cat.unlocked){
+				gs = Level3;
+				PlayerReturn();
+			};
+			
+			
+			for(int i = 0; i < buttonQuantity; i++){
+				DrawSprite(buttons[i].x, buttons[i].y, buttons[i].sprite);	
+				if(CheckCollision(buttons[i].x, buttons[i].y, 1, 1)){
+					buttons[i].y = - 128;	
+					buttonPressed++;				
+				}
+							
+			}
+				
+			/*
+			for(int i =0; i < meteorQuantity; i++){
+				
+				if(cicleCounter % (60 / meteors[i].speed) == 0){
+					meteors[i].x += tilesize * meteors[i].direction;
+					if(meteors[i].direction == 1){
+						if(CheckCollision(meteors[i].x - tilesize, meteors[i].y, meteors[i].xSize, meteors[i].ySize))
+							playerX += tilesize * meteors[i].direction;
+					}else{
+						if(CheckCollision(meteors[i].x + tilesize, meteors[i].y, meteors[i].xSize, meteors[i].ySize))
+							playerX += tilesize * meteors[i].direction;
+					}
+					
+					
+				} 
+				DrawSprite(meteors[i].x, meteors[i].y, meteors[i].sprite);
+				if(CheckCollision(meteors[i].x, meteors[i].y, meteors[i].xSize, meteors[i].ySize)) onMeteor = true;
+				
+				if(meteors[i].x <= 0 + 16 || meteors[i].x >= display_width - 16){
+					if(meteors[i].direction == 1)
+						meteors[i].x = 0 + 8
+						;
+					else
+						meteors[i].x = display_width - tilesize - 8;
+				}	
+			}*/
+			
+			//DRAW E UPDATE DOS ALEINS
+			for(int i =0; i < alienQuantity; i++){
+				if(cicleCounter % (60 / aliens[i].speed) == 0) aliens[i].x += tilesize * aliens[i].direction;
+				DrawSprite(aliens[i].x, aliens[i].y, aliens[i].sprite);
+				if(CheckCollision(aliens[i].x, aliens[i].y, aliens[i].xSize, aliens[i].ySize)) PlayerReturn(); ;
+				if(aliens[i].x <= 0 + 16 || aliens[i].x >= display_width - 16){
+					if(aliens[i].direction == 1)
+						aliens[i].x = 0 + 8;
+					else
+						aliens[i].x = display_width - tilesize - 8;
+				}	
+			}
+			PlayerInput();
+			PlayerMovement();	
+			DrawSprite(playerX, playerY, playerSheet[activeSprite]);
+			
+			if(IsPressing(key_ESCAPE)){
+				playing = false;
+				gs = MainMenu;
+			};
+			
+			if(!onMeteor){
+				PlayerReturn();
+				onMeteor = true;
+			} 
+			
+			if(buttonPressed >= buttonQuantity){
+				cat.unlocked = true;
+			}
+			
+			setactivepage(pg);
+			if(cicleCounter >= 60){
+				cicleCounter = 0;
+				
+					if(gameplayTimer[1] != 0 || gameplayTimer[0])gameplayTimer[1]--;
+					if(gameplayTimer[1] <= -1){
+						if(gameplayTimer[0] > 0) gameplayTimer[0]--;
+						gameplayTimer[1] = 9;
+					}
+			
+				
+			} 
+		}
+	}
+	free(aliens);
+	free(background);
+	free(catImage);
+	free(alienImage);
+	free(buttonImage);
+	//free(meteors);
+}
+
+
+void Level1State(){
+	
+	unsigned long tickCount;
+	unsigned long initTick;
+	
+	initTick = GetTickCount();
+	
+	int cicleCounter = 0;
+	
+	void* background = LoadSprite("map2.jpg", 1600, 900, 1, 0, 0, 1600, 900);
+	
+	
+	Alien* aliens;
+	int alienQuantity = 4;
+	aliens = NULL;	
+	void* alienImage = LoadSprite("alien.jpg", 128, 128, 1);	
+	aliens = (Alien *) realloc(aliens, sizeof(Alien) * alienQuantity);
+	aliens[0] = AddAlien(0, 5, 1, 2, alienImage, 1, 1);
+	aliens[1] = AddAlien(13, 2, -1, 3, alienImage, 1, 1);
+	aliens[2] = AddAlien(0, 4, 1, 5, alienImage, 1, 1);
+	aliens[3] = AddAlien(13, 6, 1, 4, alienImage, 1, 1);
+	
+	void* catImage = LoadSprite("gato.jpg", 128, 128);
+	Cat cat = AddCat(6, 1, catImage);
+	void* buttonImage = LoadSprite("button.jpg", 128, 128);
+	
+	
+	Button* buttons;
+	int buttonQuantity = 2;
+	int buttonPressed = 0;
+	buttons = NULL;
+	buttons = (Button *) realloc(buttons, sizeof(Button) * buttonQuantity);
+	for(int i = 0; i < buttonQuantity; i++)
+		buttons[i] = AddButton(buttonImage);
+	
+	
+	/*
+	Meteor* meteors;
+	int meteorQuantity = 4;
+	void* meteorImage = LoadSprite("car.jpg", 128, 128, 1);
+	meteors = NULL;
+	
+	meteors = (Meteor *) realloc(meteors, sizeof(Meteor) * meteorQuantity);
+	meteors[0] = AddMeteor(0, 3, 1, 2, meteorImage, 1, 1);
+	meteors[1] = AddMeteor(12, 2, -1, 3, meteorImage, 1, 1);
+	meteors[2] = AddMeteor(0, 1, 1, 4, meteorImage, 1, 1);
+	meteors[3] = AddMeteor(12, 0, -1, 1, meteorImage, 1, 1);*/
+	
+	int tileset[7] = {0, 0, 0, 0, 0, 0, 0};
+	
+	score = 0;
+	
+	gameplayTimer[0] = 6;
+	gameplayTimer[1] = 0;
+	
+	int nDigits = floor(log10(abs(139))) + 1;
+	while(gs == Level1){
+		tickCount = GetTickCount();
+		if(initTick - tickCount > 1000/60){
+			initTick = tickCount;
+			cicleCounter++;
+			SwitchPage();
+			putimage(0, 0, background, COPY_PUT);
+			
+		
+			
+			//TIMER VISUAL
+			putimage(display_width / 2-128, 0, numbers[gameplayTimer[0]], COPY_PUT);
+			putimage(display_width / 2, 0, numbers[gameplayTimer[1]], COPY_PUT);
+			
+			if(tileset[playerY / 128] == 1)
+				onMeteor = false;
+			
+			DrawSprite(cat.x, cat.y, cat.sprite);
+			//PASSAR DE FASE
+			if(CheckCollision(cat.x, cat.y, 1, 1) && cat.unlocked){
+				gs = Level2;
+				
+			};
+			
+			
+			for(int i = 0; i < buttonQuantity; i++){
+				DrawSprite(buttons[i].x, buttons[i].y, buttons[i].sprite);	
+				if(CheckCollision(buttons[i].x, buttons[i].y, 1, 1)){
+					buttons[i].y = - 128;	
+					buttonPressed++;				
+				}
+							
+			}
+				
+			/*
+			for(int i =0; i < meteorQuantity; i++){
+				
+				if(cicleCounter % (60 / meteors[i].speed) == 0){
+					meteors[i].x += tilesize * meteors[i].direction;
+					if(meteors[i].direction == 1){
+						if(CheckCollision(meteors[i].x - tilesize, meteors[i].y, meteors[i].xSize, meteors[i].ySize))
+							playerX += tilesize * meteors[i].direction;
+					}else{
+						if(CheckCollision(meteors[i].x + tilesize, meteors[i].y, meteors[i].xSize, meteors[i].ySize))
+							playerX += tilesize * meteors[i].direction;
+					}
+					
+					
+				} 
+				DrawSprite(meteors[i].x, meteors[i].y, meteors[i].sprite);
+				if(CheckCollision(meteors[i].x, meteors[i].y, meteors[i].xSize, meteors[i].ySize)) onMeteor = true;
+				
+				if(meteors[i].x <= 0 + 16 || meteors[i].x >= display_width - 16){
+					if(meteors[i].direction == 1)
+						meteors[i].x = 0 + 8
+						;
+					else
+						meteors[i].x = display_width - tilesize - 8;
+				}	
+			}*/
+			
+			//DRAW E UPDATE DOS ALEINS
+			for(int i =0; i < alienQuantity; i++){
+				if(cicleCounter % (60 / aliens[i].speed) == 0) aliens[i].x += tilesize * aliens[i].direction;
+				DrawSprite(aliens[i].x, aliens[i].y, aliens[i].sprite);
+				if(CheckCollision(aliens[i].x, aliens[i].y, aliens[i].xSize, aliens[i].ySize)) PlayerReturn(); ;
+				if(aliens[i].x <= 0 + 16 || aliens[i].x >= display_width - 16){
+					if(aliens[i].direction == 1)
+						aliens[i].x = 0 + 8;
+					else
+						aliens[i].x = display_width - tilesize - 8;
+				}	
+			}
+			PlayerInput();
+			PlayerMovement();	
+			DrawSprite(playerX, playerY, playerSheet[activeSprite]);
+			
+			if(IsPressing(key_ESCAPE)){
+				playing = false;
+				gs = MainMenu;
+			};
+			
+			if(!onMeteor){
+				PlayerReturn();
+				onMeteor = true;
+			} 
+			
+			if(buttonPressed >= buttonQuantity){
+				cat.unlocked = true;
+			}
+			
+			setactivepage(pg);
+			if(cicleCounter >= 60){
+				cicleCounter = 0;
+				
+					if(gameplayTimer[1] != 0 || gameplayTimer[0])gameplayTimer[1]--;
+					if(gameplayTimer[1] <= -1){
+						if(gameplayTimer[0] > 0) gameplayTimer[0]--;
+						gameplayTimer[1] = 9;
+					}
+			
+				
+			} 
+		}
+	}
+	free(aliens);
+	free(background);
+	free(catImage);
+	free(alienImage);
+	free(buttons);
+	free(buttonImage);
+	PlayerReturn();
+	//free(meteors);
+}
+
+
 
 int main(){
 	
@@ -453,6 +852,8 @@ int main(){
 	playerSheet = LoadSpriteSheet(4, "sapo.jpg", 1536, 128);
 	
 	numbers = LoadSpriteSheet(10, "numbers.jpg", 1280, 128);
+	
+	srand(time(NULL));
 		
 	int x =0;
 	while(playing){
@@ -461,8 +862,14 @@ int main(){
 			case(MainMenu):	
 				MainMenuState();			
 				break;		
-			case(Gameplay):
-				GameplayState();
+			case(Level1):
+				Level1State();
+				break;
+			case(Level2):
+				Level2State();
+				break;
+			case(Level3):
+				Level3State();
 				break;
 		}
 					
